661233aee2b1d4c24c8cd7b331be4b7453ac05a5
Lolitre <lolitre54@gmail.com>
2019-06-07 17:10:59 +0200

    Test concluant -> finir les autres Implémentations et s'occuper de l'injection de la datasource

--- src/main/java/fr/llescoffit/DAO/Impl/AbstractDAOImpl.java
+++ src/main/java/fr/llescoffit/DAO/Impl/AbstractDAOImpl.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
+
 public abstract class AbstractDAOImpl {
 
     private DataSource dataSource;
@@ -12,10 +13,11 @@ public abstract class AbstractDAOImpl {
 
         DriverManagerDataSource source = new DriverManagerDataSource();
         source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        source.setUrl("jdbc:mysql://localhost:3306/siteescalade");
+        source.setUrl("jdbc:mysql://localhost:3306/siteescalade ?useUnicode=true\n" +
+                "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&\n" +
+                "serverTimezone=UTC");
         source.setUsername("root");
         source.setPassword("Lolfolder");
-
         return dataSource;
     }
 
--- src/main/java/fr/llescoffit/DAO/Impl/UserImpl.java
+++ src/main/java/fr/llescoffit/DAO/Impl/UserImpl.java
@@ -4,17 +4,28 @@ import fr.llescoffit.DAO.Mapper.UserMapper;
 import fr.llescoffit.DAO.UserDAO;
 import fr.llescoffit.Model.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 public class UserImpl extends AbstractDAOImpl implements UserDAO {
 
+
     // Ajout d'un utilisateur dans la base de données.
     public void add(User user){
 
+            DriverManagerDataSource source = new DriverManagerDataSource();
+            source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            source.setUrl("jdbc:mysql://localhost:3306/siteescalade ?userUniqcode= true &&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false& serverTimezone=UTC");
+            source.setUsername("root");
+            source.setPassword("Lolfolder");
+            System.out.println("Test initialisation data Source");
+
         String rsql
                 = "INSERT INTO User VALUES ('"+user.getEmail()+"', '"+user.getMotDePasse()+"', '"+user.getNom()+"' ) ";
-        JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+
+        JdbcTemplate vJdbcTemplate = new JdbcTemplate(source);
         vJdbcTemplate.update(rsql);
 
     }
